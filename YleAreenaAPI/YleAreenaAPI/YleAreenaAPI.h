938e1df578fbdd3541fd8231ad0652a66d242b78
Juhani Vanhala <juhani@dcs8-0yyb5ncwv2hp8cjy-3.rev.dnainternet.fi>
2016-01-31 22:21:48 +0200

    Initial Commit

---
+++ YleAreenaAPI/YleAreenaAPI/YleAreenaAPI.h
@@ -0,0 +1,19 @@
+//
+//  YleAreenaAPI.h
+//  YleAreenaAPI
+//
+//  Created by Juhani Vanhala on 31.1.2016.
+//  Copyright © 2016 Juhani Vanhala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YleAreenaAPI.
+FOUNDATION_EXPORT double YleAreenaAPIVersionNumber;
+
+//! Project version string for YleAreenaAPI.
+FOUNDATION_EXPORT const unsigned char YleAreenaAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YleAreenaAPI/PublicHeader.h>
+
+
